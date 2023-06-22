2dff5f8b6ebbe1cbd43b1e89b9cae6185cdf0bef
iturres <emanueliturres.co@gmail.com>
2023-06-22 15:09:18 -0300

    Remove JSON.parse() on 'localStorage SET' in 'gameId.js' -script-

--- src/js/services/gameId.js
+++ src/js/services/gameId.js
@@ -24,7 +24,7 @@ const gameId = async () => {
       .split(': ')[1]
       .replace(' added.', '');
 
-    localStorage.setItem('gameId', JSON.parse(newGameId));
+    localStorage.setItem('gameId', newGameId);
   } catch (error) {
     throw new Error(`Failed to create a new game. Error => ${error}`);
   }
