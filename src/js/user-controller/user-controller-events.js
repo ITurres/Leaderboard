407052a238453ea06cc65976f0d852c90b2734fd
iturres <emanueliturres.co@gmail.com>
2023-06-22 15:31:16 -0300

    Added Empty values of 'inputs' and 'holders' on 'user-controller-events.js' -events-handler- -script-

--- src/js/user-controller/user-controller-events.js
+++ src/js/user-controller/user-controller-events.js
@@ -15,12 +15,16 @@ document
       user: userName.value,
       score: userScore.value
     });
+
+    userName.value = '';
+    userScore.value = '';
   });
 
 document
   .querySelector('[data-get-all-scores]')
   .addEventListener('click', async () => {
     const scoresHolder = document.querySelector('[data-scores-holder]');
+    scoresHolder.innerHTML = '';
     const userServices = new UserServices();
     const scores = await userServices.getAllScores();
     injectScoresOnDOM(scoresHolder, scores);
