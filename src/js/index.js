e986cb1d8eae673ef1e90ebe7a469edf31dc23a2
iturres <emanueliturres.co@gmail.com>
2023-06-22 20:47:42 -0300

    Import 'gameId' to 'index.js' -script- and check 'localStorage size'

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import '../styles/index.scss';
 import './user-controller/user-controller-events.js';
 import leadorboardIcon from '../assets/media/leaderboard-icon.png';
+import gameId from './services/gameId.js';
 
 document.getElementById('logo').src = leadorboardIcon;
+
+if (window.localStorage.length <= 0) {
+  await gameId();
+}
