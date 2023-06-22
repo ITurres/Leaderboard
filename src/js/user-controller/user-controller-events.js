f082cab9097813af66bc94a4d05d6a69c881614d
iturres <emanueliturres.co@gmail.com>
2023-06-21 23:37:51 -0300

    Add 'user-controller-events.js' -script- to handle user events

---
+++ src/js/user-controller/user-controller-events.js
@@ -0,0 +1,27 @@
+import UserServices from '../services/userServices.js';
+import injectScoresOnDOM from '../markup-injectors/injectScoresOnDOM.js';
+
+document
+  .querySelector('[data-submit-new-score]')
+  .addEventListener('click', async () => {
+    const userName = document.querySelector('input[name="name"]');
+    const userScore = document.querySelector('input[name="score"]');
+
+    if (!userName.value && !userScore.value) {
+      return;
+    }
+    const userServices = new UserServices();
+    await userServices.addScore({
+      user: userName.value,
+      score: userScore.value
+    });
+  });
+
+document
+  .querySelector('[data-get-all-scores]')
+  .addEventListener('click', async () => {
+    const scoresHolder = document.querySelector('[data-scores-holder]');
+    const userServices = new UserServices();
+    const scores = await userServices.getAllScores();
+    injectScoresOnDOM(scoresHolder, scores);
+  });
