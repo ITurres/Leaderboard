30568cc0e506faae78e855912360fcd38f0693ec
iturres <emanueliturres.co@gmail.com>
2023-06-22 14:54:41 -0300

    Import 'leaderboard logo' onto 'index.js' -script-

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
 import '../styles/index.scss';
 import './user-controller/user-controller-events.js';
+import leadorboardIcon from '../assets/media/leaderboard-icon.png';
+
+document.getElementById('logo').src = leadorboardIcon;
